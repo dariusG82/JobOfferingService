9023832b58ea17a462b011078041cb3afc589e64
dariusG82 <darius.govedas@gmail.com>
2022-07-10 15:39:15 +0300

    Initial commit

---
+++ src/main/java/eu/dariusgovedas/jobofferingservice/JobOfferingServiceApplication.java
@@ -0,0 +1,13 @@
+package eu.dariusgovedas.jobofferingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOfferingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobOfferingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/dariusgovedas/jobofferingservice/JobOfferingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.dariusgovedas.jobofferingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobOfferingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
