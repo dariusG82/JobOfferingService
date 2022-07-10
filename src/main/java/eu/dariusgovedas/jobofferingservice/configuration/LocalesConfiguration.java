e26eb76e2413a7329e8ae56a628e784405ded011
dariusG82 <darius.govedas@gmail.com>
2022-07-10 18:25:38 +0300

    13 - Added signUpForm and Locales Configurations. Created En/LT messages files

---
+++ src/main/java/eu/dariusgovedas/jobofferingservice/configuration/LocalesConfiguration.java
@@ -0,0 +1,31 @@
+package eu.dariusgovedas.jobofferingservice.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class LocalesConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        LocaleResolver localeResolver = new SessionLocaleResolver();
+
+        return localeResolver;
+    }
+
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
--- src/main/resources/db/init.sql
+++ src/main/resources/db/init.sql
@@ -1,14 +1,71 @@
-INSERT INTO JOB_DETAILS (id, description, salary) VALUES (1, 'Very good job', 2025);
-INSERT INTO JOB (jobid, job_title, job_type, deadline, details_id) VALUES ('00000000-0000-0000-0000-000000000001', 'Java', 'Coding','2022-07-05', 1);
-INSERT INTO JOB_DETAILS (id, description, salary) VALUES (2, 'Very good job', 4025);
-INSERT INTO JOB (jobid, job_title, job_type, deadline, details_id) VALUES ('00000000-0000-0000-0000-000000000003', 'Java', 'Coding','2022-08-05', 2);
-INSERT INTO JOB_DETAILS (id, description, salary) VALUES (3, 'Very good job', 6025);
-INSERT INTO JOB (jobid, job_title, job_type, deadline, details_id) VALUES ('00000000-0000-0000-0000-000000000002', 'Java', 'Coding','2022-09-05', 3);
-INSERT INTO JOB_DETAILS (id, description, salary) VALUES (4, 'Very good job', 3355);
-INSERT INTO JOB (jobid, job_title, job_type, deadline, details_id) VALUES ('00000000-0000-0000-0000-000000000004', 'C++', 'Coding','2022-10-05', 4);
-INSERT INTO JOB_DETAILS (id, description, salary) VALUES (5, 'Very good job', 2225);
-INSERT INTO JOB (jobid, job_title, job_type, deadline, details_id) VALUES ('00000000-0000-0000-0000-000000000005', 'Java', 'Coding','2022-11-05', 5);
-INSERT INTO JOB_DETAILS (id, description, salary) VALUES (6, 'good job', 3025);
-INSERT INTO JOB (jobid, job_title, job_type, deadline, details_id) VALUES ('00000000-0000-0000-0000-000000000006', 'Java', 'Coding','2022-12-05', 6);
-INSERT INTO JOB_DETAILS (id, description, salary) VALUES (7, 'Very job', 525);
-INSERT INTO JOB (jobid, job_title, job_type, deadline, details_id) VALUES ('00000000-0000-0000-0000-000000000007', 'Kotlin', 'Coding','2023-01-05', 7);
+INSERT INTO RECRUITER (id, business_name)
+VALUES (1, 'buss1');
+INSERT INTO RECRUITER (id, business_name)
+VALUES (2, 'buss2');
+INSERT INTO RECRUITER (id, business_name)
+VALUES (3, 'buss3');
+
+INSERT INTO FREELANCER (id, jobs_finished, rating, total_income)
+VALUES (1, 30, 4.9, 4250.00);
+INSERT INTO FREELANCER (id, jobs_finished, rating, total_income)
+VALUES (2, 60, 8.9, 12995.00);
+INSERT INTO FREELANCER (id, jobs_finished, rating, total_income)
+VALUES (3, 15, 6.5, 3590.00);
+
+INSERT INTO APP_USER (id, username, password, name, surname, email_address, phone_number, freelancer_id, recruiter_id)
+VALUES (1, 'business1', 'pass1', 'Darius', 'G', 'dariusg@mail.com', '+370 333 35855', 1, null);
+INSERT INTO APP_USER (id, username, password, name, surname, email_address, phone_number, freelancer_id, recruiter_id)
+VALUES (2, 'business2', 'pass2', 'Saulius', 'S', 'sausasu@mail.com', '+370 112 12345', 2, null);
+INSERT INTO APP_USER (id, username, password, name, surname, email_address, phone_number, freelancer_id, recruiter_id)
+VALUES (3, 'business3', 'pass3', 'Joana', 'J', 'janbej@mail.com', '+370 568 22895', null, 1);
+INSERT INTO APP_USER (id, username, password, name, surname, email_address, phone_number, freelancer_id, recruiter_id)
+VALUES (4, 'business4', 'pass4', 'Gitana', 'Z', 'zerog@gmail.com', '+370 552 05874', 3, null);
+INSERT INTO APP_USER (id, username, password, name, surname, email_address, phone_number, freelancer_id, recruiter_id)
+VALUES (5, 'business5', 'pass5', 'Kristina', 'K', 'kritti@mail.com', '+370 152 00256', null, 2);
+INSERT INTO APP_USER (id, username, password, name, surname, email_address, phone_number, freelancer_id, recruiter_id)
+VALUES (6, 'business6', 'pass6', 'Svetlana', 'K', 'svet@gmail.com', '+370 655 00112', null, 3);
+
+INSERT INTO JOB_DETAILS (description, salary, id)
+VALUES ('Very good job', 2500.00, 1);
+INSERT INTO JOB (deadline, details_id, recruiter_id, freelancer_id, job_title, job_type, jobid)
+VALUES ('2022-05-01', 1, 1, 1, 'FasJob', 'Java', '00000000-0000-0000-0000-000000000001');
+INSERT INTO JOB_DETAILS (description, salary, id)
+VALUES ('Very good job', 1500.00, 2);
+INSERT INTO JOB (deadline, details_id, recruiter_id, freelancer_id, job_title, job_type, jobid)
+VALUES ('2022-08-01', 2, 2, null, 'FasterJob', 'Kotlin', '00000000-0000-0000-0000-000000000002');
+INSERT INTO JOB_DETAILS (description, salary, id)
+VALUES ('Very good job', 2200.00, 3);
+INSERT INTO JOB (deadline, details_id, recruiter_id, freelancer_id, job_title, job_type, jobid)
+VALUES ('2022-07-01', 3, 1, null, 'FastestJob', 'Maven', '00000000-0000-0000-0000-000000000003');
+INSERT INTO JOB_DETAILS (description, salary, id)
+VALUES ('Very good job', 4200.00, 4);
+INSERT INTO JOB (deadline, details_id, recruiter_id, freelancer_id, job_title, job_type, jobid)
+VALUES ('2022-09-01', 4, 3, 1, 'FSlowJob', 'C++', '00000000-0000-0000-0000-000000000004');
+INSERT INTO JOB_DETAILS (description, salary, id)
+VALUES ('Very good job', 900.00, 5);
+INSERT INTO JOB (deadline, details_id, recruiter_id, freelancer_id, job_title, job_type, jobid)
+VALUES ('2022-11-01', 5, 2, 3, 'VerySlowJob', 'C#', '00000000-0000-0000-0000-000000000005');
+INSERT INTO JOB_DETAILS (description, salary, id)
+VALUES ('Very good job', 1200.00, 6);
+INSERT INTO JOB (deadline, details_id, recruiter_id, freelancer_id, job_title, job_type, jobid)
+VALUES ('2022-10-01', 6, 2, null, 'FasJob', 'Java', '00000000-0000-0000-0000-000000000006');
+INSERT INTO JOB_DETAILS (description, salary, id)
+VALUES ('Very good job', 3350.00, 7);
+INSERT INTO JOB (deadline, details_id, recruiter_id, freelancer_id, job_title, job_type, jobid)
+VALUES ('2022-05-21', 7, 3, 2, 'FasJob', 'Python', '00000000-0000-0000-0000-000000000007');
+INSERT INTO JOB_DETAILS (description, salary, id)
+VALUES ('Very good job', 25100.00, 8);
+INSERT INTO JOB (deadline, details_id, recruiter_id, freelancer_id, job_title, job_type, jobid)
+VALUES ('2022-05-11', 8, 1, null, 'FasJob', 'JUnit', '00000000-0000-0000-0000-000000000008');
+INSERT INTO JOB_DETAILS (description, salary, id)
+VALUES ('Very good job', 200.00, 9);
+INSERT INTO JOB (deadline, details_id, recruiter_id, freelancer_id, job_title, job_type, jobid)
+VALUES ('2022-06-11', 9, 2, null, 'FasJob', 'Git', '00000000-0000-0000-0000-000000000009');
+INSERT INTO JOB_DETAILS (description, salary, id)
+VALUES ('Very good job', 2800.00, 10);
+INSERT INTO JOB (deadline, details_id, recruiter_id, freelancer_id, job_title, job_type, jobid)
+VALUES ('2022-02-11', 10, 1, null, 'FasJob', 'Kotlin', '00000000-0000-0000-0000-000000000010');
+INSERT INTO JOB_DETAILS (description, salary, id)
+VALUES ('Very good job', 3500.00, 11);
+INSERT INTO JOB (deadline, details_id, recruiter_id, freelancer_id, job_title, job_type, jobid)
+VALUES ('2023-04-01', 11, 2, 3, 'FasJob', 'Android', '00000000-0000-0000-0000-000000000011');
