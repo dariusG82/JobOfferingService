79a1a90f6eb7e7f997f129e79e244ca25b1d36ac
dariusG82 <darius.govedas@gmail.com>
2022-07-25 22:49:50 +0300

    41 - Updated JobDetails with GeneratedID

--- src/main/java/eu/dariusgovedas/jobofferingservice/jobs/entities/JobDetails.java
+++ src/main/java/eu/dariusgovedas/jobofferingservice/jobs/entities/JobDetails.java
@@ -1,12 +1,12 @@
 package eu.dariusgovedas.jobofferingservice.jobs.entities;
 
-import eu.dariusgovedas.jobofferingservice.jobs.entities.Job;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import java.math.BigDecimal;
@@ -19,7 +19,8 @@ import java.math.BigDecimal;
 public class JobDetails {
 
     @Id
-    private long id;
+    @GeneratedValue
+    private Long id;
     private String description;
     private BigDecimal salary;
 
