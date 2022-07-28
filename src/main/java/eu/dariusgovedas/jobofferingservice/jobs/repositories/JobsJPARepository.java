471640acd0f20dcd593c5cb235a9d47008eaff0b
dariusG82 <darius.govedas@gmail.com>
2022-07-28 23:49:57 +0300

    57 - Updated JobsRepository queries

--- src/main/java/eu/dariusgovedas/jobofferingservice/jobs/repositories/JobsJPARepository.java
+++ src/main/java/eu/dariusgovedas/jobofferingservice/jobs/repositories/JobsJPARepository.java
@@ -4,20 +4,17 @@ import eu.dariusgovedas.jobofferingservice.jobs.entities.Job;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
-import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface JobsJPARepository extends JpaRepository <Job, UUID> {
 
-    @Query("FROM Job j WHERE j.freelancer IS NULL OR j.freelancer.id <> :id AND UPPER(j.jobTitle) LIKE %:title%")
-    Page<Job> findInAvailableJobs(@Param("title") String title, @Param("id") Long freelancerID, Pageable pageable);
+    @Query("FROM Job j WHERE j.rating IS NULL AND j.freelancer IS NULL AND UPPER(j.jobTitle) LIKE %:title%")
+    Page<Job> findInAvailableJobs(@Param("title") String title, Pageable pageable);
 
     @Query("FROM Job j WHERE j.recruiter.id=:id AND UPPER(j.jobTitle) LIKE %:title%")
     Page<Job> findInUserJobs(@Param("title") String title, @Param("id") Long id, Pageable pageable);
